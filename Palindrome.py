6cefad68bd3426b38d104d30fdec6daf7c016e16
Amee Karelia <Amee.karelia16@gmail.com>
2022-06-25 14:15:01 -0700

    palindrome solution

--- Palindrome.py
+++ Palindrome.py
@@ -1,16 +1,20 @@
+#  check if the number is palindrome.
+#  this slution only itrates through have of the length of the given number.
+
+
 def palindrome(x):
-    if x < 0:
+    if x < 0 or (x >0 and x%10 == 0):
         return False
-    y = x
     result = 0
-    while y > 0:
-        z= y %10
-        y = y//10
-        result = (result*10) + z
+    while result < x:
+        result = (result*10) + x%10
+        x = x//10
+        print(x)
+        print(result)
         
-    if x == result:
+    if (x == result or x == result//10):
         return True
     else:
         return False
-
-print(palindrome(121))
+   
+print(palindrome(10))
