c7c89b341450a0076b3b92ac8f42eee91ed1e4fd
Amee Karelia <Amee.karelia16@gmail.com>
2022-07-15 23:09:11 -0700

    new problem

--- Valid_prentheses/README.md
+++ Valid_prentheses/README.md
@@ -6,6 +6,6 @@ https://leetcode.com/problems/valid-parentheses/
 used hashmaps and stack data structure to solve this problem.
 
 Time complexity : O(n)
-Space complexity : O(n)O(n) 
+Space complexity : O(n) 
 
 
--- Valid_prentheses/solution.py
+++ Valid_prentheses/solution.py
@@ -1,40 +1,19 @@
-# def isValid(str):
-#     dict= {")":"(", "}": "{", "]": "["}
-#     stack = []
-#     for i in str:
-#         print(i)
-#         if i in dict:
-#             if stack:
-#                 top_val= stack.pop() if stack else "#"
-#             if top_val != dict[i]:
-#                 return False   
-            
-#         else:
-#             stack.append(i)
-#     if not stack:
-#         return True
-#     else:
-#         return False
-
-
-# print(isValid("]"))
-
 
 def isValid(str):
-            dict = {")":"(", "}":"{","]":"[" }
-            stack = []
-            for i in str:
-                # if i is an closing bracket
-                if i in dict:
-                    top_value = stack.pop() if stack else "#"
-                    if dict[i] != top_value:
-                        return False
-                else:
-                    stack.append(i)
-                    print(stack)
-            if not stack:
-                return True
+        dict = {")":"(", "}":"{","]":"[" }
+        stack = []
+        for i in str:
+            # if i is an closing bracket
+            if i in dict:
+                top_value = stack.pop() if stack else "#"
+                if dict[i] != top_value:
+                    return False
             else:
-                return False
+                stack.append(i)
+                print(stack)
+        if not stack:
+            return True
+        else:
+            return False
 
 print(isValid("]"))
