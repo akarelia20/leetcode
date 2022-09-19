025a9fad089bac391aa1704d0f277b877d702499
Amee Karelia <Amee.karelia16@gmail.com>
2022-09-18 23:28:49 -0700

    solved using binary search

---
+++ 69_Sqrt(x)/solution.py
@@ -0,0 +1,12 @@
+def mySqrt(x):
+    l, r = 0,x
+    while l <= r:
+        mid = (r+l)//2
+        if mid* mid <= x < (mid+1)*(mid+1):
+            return mid
+        elif mid*mid > x:
+            r = mid-1
+        else:
+            l = mid+1
+
+print(mySqrt(8))
