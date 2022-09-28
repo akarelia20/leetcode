76678e14553623ac47f3d3b90a02234190ea1913
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2022-09-28 16:38:59 -0700

    Time: 59 ms (45.90%), Space: 13.9 MB (59.12%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    # from collections import deque
+    def countGoodSubstrings(self, s: str) -> int:
+        count= 0
+        queue = deque(list(s))
+        
+        while len(queue) >= 3:
+            left = queue.popleft()
+            middle = queue[0]
+            right = queue[1]
+            
+            if left != right and right != middle and left!= middle:
+                count+=1
+        return count
+                
+        
