b34394f0ec0c1be225d63557c7cd7fbac79b8fbf
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2022-11-06 13:23:40 -0800

    Time: 1388 ms (32.82%), Space: 49.4 MB (77.66%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -9,11 +9,28 @@ class Solution:
         if root is None:
             return 0
         
-        if root.right == None and root.left == None:
-            return 1
-        if root.right and not root.left:
-            return 1+(self.minDepth(root.right))
-        if root.left and not root.right:
-            return 1+(self.minDepth(root.left))
+        queue = collections.deque()
+        queue.append((root,1))
         
-        return 1+min(self.minDepth(root.right), self.minDepth(root.left))
+        while queue:
+            node, depth = queue.popleft()
+            
+            if node.right == None and node.left== None:
+                return depth
+            if node.right:
+                queue.append((node.right, depth+1))
+            if node.left:
+                queue.append((node.left, depth+1))
+                
+# def minDepthDFS(self, root: Optional[TreeNode]) -> int:
+#         if root is None:
+#             return 0
+        
+#         if root.right == None and root.left == None:
+#             return 1
+#         if root.right and not root.left:
+#             return 1+(self.minDepth(root.right))
+#         if root.left and not root.right:
+#             return 1+(self.minDepth(root.left))
+        
+#         return 1+min(self.minDepth(root.right), self.minDepth(root.left))
